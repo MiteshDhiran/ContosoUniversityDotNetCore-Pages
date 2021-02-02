7a9067b31f20b2a33daf6a079d741d301ba6a31d
Mrudul Dhiran <mrudul.dhiran@gmail.com>
2021-02-02 16:40:20 +0530

    Refactored - removed dead code

--- ContosoUniversity/Pages/Instructors/Details.cshtml.cs
+++ ContosoUniversity/Pages/Instructors/Details.cshtml.cs
@@ -31,19 +31,8 @@ namespace ContosoUniversity.Pages.Instructors
         }
 
         public Model Data { get; private set; }
-        //new ContosoAPIContext(new ContosoContext(_db, _configuration))
-        /*
-        public async Task OnGetAsync(Query query) =>
-            Data = await Task.FromResult(query.Process(_apiContext)
-                .Result.TryGetResult(out var m)
-                ? m
-                : null);
-                */
-        //await Task.FromResult((Model)null) ; //await _mediator.Send(query);
 
-        //public async Task OnGetAsync(Query query) => Data = await query.ProcessRequest(_apiContext);
         public async Task OnGetAsync(Query query) => Data = await query.Process(_apiContext);
-
 
         public class Query : IRequestWithFluentValidator<int?, Model, ContosoContext> 
         {
@@ -54,41 +43,23 @@ namespace ContosoUniversity.Pages.Instructors
             public Func<IRequestContext<int?, Model, ContosoContext>, MayBe<ValidationException>> ValidationFunc
                 => (reqContext) =>
                 {
-                    QueryValidator validator = new QueryValidator();
+                    var validator = new QueryValidator();
                     var validationResult = validator.Validate(this);
-                    if (validationResult.IsValid)
-                    {
-                        return new MayBe<ValidationException>(MayBeDataState.DataNotPresent);
-                    }
-                    else
-                    {
-                        var validationException = new ValidationException(validationResult.Errors);
-                        return new MayBe<FluentValidation.ValidationException>(validationException);
-                    }
+                    return validationResult.IsValid
+                    ? new MayBe<ValidationException>(MayBeDataState.DataNotPresent)
+                    : new MayBe<FluentValidation.ValidationException>(new ValidationException(validationResult.Errors));
+                    
                 };
 
             public Task<Model> Process(IAPIContext<ContosoContext> context) => 
                 ((IRequestWithFluentValidator<int?, Model, ContosoContext>)this).InterfaceProcess(context);
 
-            internal class QueryValidator : AbstractValidator<Query>
+            class QueryValidator : AbstractValidator<Query>
             {
                 public QueryValidator()
                 {
-                    RuleFor(m => m.Id).NotNull();
+                    RuleFor(m => m.Data).NotNull();
                 }
-            }
-            
-        }
-
-        
-
-
-        public class Validator : AbstractValidator<Query>
-        {
-            public Validator()
-            {
-                //RuleFor(m => m.Id).GreaterThan(100);
-                RuleFor(m => m.Id).NotNull();
             }
         }
 
@@ -111,24 +82,6 @@ namespace ContosoUniversity.Pages.Instructors
         {
             public MappingProfile() => CreateMap<Instructor, Model>();
         }
-        /*
-        public class Handler : IRequestHandler<Query, Model>
-        {
-            private readonly SchoolContext _db;
-            private readonly IConfigurationProvider _configuration;
-
-            public Handler(SchoolContext db, IConfigurationProvider configuration)
-            {
-                _db = db;
-                _configuration = configuration;
-            }
-
-            public Task<Model> Handle(Query message, CancellationToken token) => _db
-                .Instructors
-                .Where(i => i.Id == message.Id)
-                .ProjectTo<Model>(_configuration)
-                .SingleOrDefaultAsync(token);
-        }
-        */
+        
     }
 }
